52fa27f5b7612ab54deddf9f5d942436afcdd510
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-09-21 17:26:47 +0900

    n2739

---
+++ src/class1/n2739/Main.java
@@ -0,0 +1,15 @@
+package class1.n2739;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
+}
