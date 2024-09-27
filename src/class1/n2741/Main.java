ebfa7b633135f31be6f3b9e6345f283054a8d1f7
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-09-27 21:23:44 +0900

    n2741 Stringbuilder

--- src/class1/n2741/Main.java
+++ src/class1/n2741/Main.java
@@ -6,21 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
 
-        // Your main program logic goes here
         int N = sc.nextInt();
         if (N <= 100_000) {
+            StringBuilder sb = new StringBuilder();
             for (int i = 1; i <= N; i++) {
-                System.out.println(i);
+                sb.append(i).append("\n");
             }
+            System.out.println(sb);
         } else {
             System.out.println("error");
         }
 
-//        long endTime = System.currentTimeMillis();
-//
-//        long timeTaken = endTime - startTime;
-//        System.out.println("Time: " + timeTaken + "ms");
+        long endTime = System.currentTimeMillis();
+
+        long timeTaken = endTime - startTime;
+        System.out.println("Time: " + timeTaken + "ms");
     }
 }
